10f0629cd89de5796a7677153c836c273622ce86
daniastu06 <astudillori.danie@student.ccm.edu>
2026-09-22 14:23:39 -0400

    almost completing lab1

--- Employee.java
+++ Employee.java
@@ -1,3 +1,19 @@
 public class Employee {
-    
+    private String name;
+    private int idNumber;
+    private String deparment;
+    private String position;
+
+    public Employee (String name, int idNumber, String deparment, String position){
+        this.name = name;
+        this.idNumber = idNumber;
+        this.deparment = deparment;
+        this.position = position;
+    }
+
+    public void DisplayInfo (){
+        System.out.println(name + "\t" + idNumber + "\t" + deparment + "\t" + position);
+    }
+
 }
+    
--- EmployeeTest.java
+++ EmployeeTest.java
@@ -1,3 +1,13 @@
 public class EmployeeTest {
-    
+    public static void main(String[] args) {
+        Employee employee1 = new Employee("Susan Myers", 17899, "Accountig", "Vicepresident" );
+
+        Employee employee2 = new Employee("Mark Jones", 39119, "IT0", "Programmer");
+
+        Employee employee3 = new Employee("Joy Rogers", 81774, " Manufacturing", "Engineer");
+
+        employee1.DisplayInfo();
+        employee2.DisplayInfo();
+        employee3.DisplayInfo();
+    }
 }
