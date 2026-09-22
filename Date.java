d1f2979ddb59caa3457ad4db860cefb293a18948
daniastu06 <astudillori.danie@student.ccm.edu>
2026-09-22 13:00:51 -0400

    almost completing lab1

--- Date.java
+++ Date.java
@@ -1,3 +1,39 @@
 public class Date {
+    private int month;
+    private int day;
+    private int year;
     
+    public Date(int month, int day, int year) {
+        if (month >= 1 && month <= 12){
+            this.month = month;
+        } else {
+            System.out.println("Invalid date");
+        }
+        if (day >= 1 && day <= 31){
+            this.day = day;
+        } else{
+            System.out.println("Invalid date");
+        }
+        this.year = year;
+    }
+
+    public void dateNumb(){
+        System.out.println(month + "/" + day + "/" + year);
+    }
+
+    public void dateLett(){
+        String[] months = {"", "January", "February", "March", "April", "May",
+                            "June", "July", "August", "September", "October", 
+                            "November", "December"};
+        
+        System.out.println(months[month] + " " + day + "," + year);                
+    }
+
+    public void dateLett2(){
+        String[] months = {"", "January", "February", "March", "April", "May",
+                            "June", "July", "August", "September", "October", 
+                            "November", "December"};
+        
+        System.out.println(day + " " + months[month] + " " + year);                
+    }
 }
--- DateTest.java
+++ DateTest.java
@@ -1,3 +1,23 @@
+import java.util.Scanner;
+
 public class DateTest {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter month: ");
+        int month = input.nextInt();
+
+        System.out.println("Enter day: ");
+        int day = input.nextInt();
+
+        System.out.println("Enter year: ");
+        int year = input.nextInt();
+
+        Date date = new Date(month, day, year);
+            date.dateNumb();
+            date.dateLett();
+            date.dateLett2();
+    
+    }
     
 }
